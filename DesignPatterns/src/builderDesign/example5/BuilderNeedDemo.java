14bf2bba18b6e831d94f81470aa42cba59a02a95
Thiru538 <tk.july8@gmail.com>
2020-07-12 14:37:58 +0530

    Modified builder pattern example.

--- DesignPatterns/src/builderDesign/example5/BuilderNeedDemo.java
+++ DesignPatterns/src/builderDesign/example5/BuilderNeedDemo.java
@@ -1,7 +1,7 @@
 package builderDesign.example5;
 
 //Driver class 
-public class BuilderNeedDemo { 
+public class BuilderNeedDemo {  
 	public static void main(String args[])  { 
 	 	StudentReceiver sr = new StudentReceiver(); 
 	 	System.out.println(sr.getStudent()); 
